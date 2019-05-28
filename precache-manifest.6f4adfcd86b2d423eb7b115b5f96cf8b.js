93465181d6e752e0cabeb196d480060e3e294a01
Martha Navarro <tita1089@gmail.com>
2019-05-28 00:23:29 -0500

    Updates

---
+++ precache-manifest.6f4adfcd86b2d423eb7b115b5f96cf8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ccfed9e410b7d2d014dbd0739da127",
+    "url": "/GDL002-burger-queen/./index.html"
+  },
+  {
+    "revision": "d746708a3df62116659d",
+    "url": "/GDL002-burger-queen/./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "dd4554bfec743d1281ba",
+    "url": "/GDL002-burger-queen/./static/css/main.910f92df.chunk.css"
+  },
+  {
+    "revision": "d746708a3df62116659d",
+    "url": "/GDL002-burger-queen/./static/js/2.4734a7ee.chunk.js"
+  },
+  {
+    "revision": "dd4554bfec743d1281ba",
+    "url": "/GDL002-burger-queen/./static/js/main.3f620567.chunk.js"
+  },
+  {
+    "revision": "5518386b4423cc379155",
+    "url": "/GDL002-burger-queen/./static/js/runtime~main.c68b016f.js"
+  }
+]);
